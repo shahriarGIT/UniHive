fff1b36a7eed6acc6cccbf79ebb815680a1c3e1c
ShahriarGIT <antarx11@gmail.com>
2025-05-09 20:27:27 +0100

    navbar title upated

--- src/app/components/navbar/SearchBar.jsx
+++ src/app/components/navbar/SearchBar.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  return (
+    <div className="text-2xl font-bold">
+      {" "}
+      <Link
+        href="/dashboard"
+        className="hover:font-bold text-black font-bold py-2 px-4 "
+      >
+        UniHive
+      </Link>{" "}
+    </div>
+  );
 };
 
 export default SearchBar;
--- src/app/dashboard/polling/page.jsx
+++ src/app/dashboard/polling/page.jsx
@@ -174,9 +174,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        🎯 Real-time Polling App
-      </h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Polling Dashboard</h1>
 
       {/* Users in Room */}
       {usersInRoom.length > 0 && (
